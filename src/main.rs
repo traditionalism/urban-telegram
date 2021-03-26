e6e4f834dee4d77da4bec7a7d09ddbbc50377a50
Lucas <genevasterm@protonmail.ch>
2021-03-26 13:18:57 -0500

    tweak: added more primitive app-building variables

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
-use bevy::prelude::*;
+use bevy::prelude::{App, WindowDescriptor};
+use bevy::DefaultPlugins;
 
 fn main() {
-    App::build().run();
-    println!("Hello, world!");
+    App::build()
+        .insert_resource(WindowDescriptor {
+            width: 1920.,
+            height: 1080.,
+            title: "urban-telegram".to_string(),
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .run();
 }

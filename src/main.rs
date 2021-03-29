70a411ee437b0bd0527fdf18045e4f955ea00c35
Lucas <genevasterm@protonmail.ch>
2021-03-29 04:52:21 -0500

    feat(asset/models): added 3D GLTF rendering (using GLTF SheenChair e.g)

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,48 @@
-#![windows_subsystem = "windows"]
-
-use bevy::{input::system::exit_on_esc_system, prelude::*};
+use bevy::{pbr::AmbientLight, input::system::exit_on_esc_system, prelude::*};
 use bevy::DefaultPlugins;
 
 fn main() {
     App::build()
         .insert_resource(WindowDescriptor {
             title: "urban-telegram".to_string(),
-            width: 1920.,
-            height: 1080.,
+            width: 1600.,
+            height: 1600.,
             vsync: true,
             resizable: false,
             ..Default::default()
         })
+        .insert_resource(AmbientLight {
+            color: Color::WHITE,
+            brightness: 1.0 / 5.0f32,
+        })
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
+        .add_system(rotator_system.system())
         .add_system(exit_on_esc_system.system())
         .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_scene(asset_server.load("models/sheen/SheenChair.gltf#Scene0"));
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(0.9, 0.9, 1.0).looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::Y),
+        ..Default::default()
+    });
+    commands
+        .spawn_bundle(LightBundle {
+            transform: Transform::from_xyz(3.0, 5.0, 3.0),
+            ..Default::default()
+        })
+        .insert(Rotates);
+}
+
+struct Rotates;
+
+fn rotator_system(time: Res<Time>, mut query: Query<&mut Transform, With<Rotates>>) {
+    for mut transform in query.iter_mut() {
+        *transform = Transform::from_rotation(Quat::from_rotation_y(
+            (4.0 * std::f32::consts::PI / 20.0) * time.delta_seconds(),
+        )) * *transform;
+    }
 }

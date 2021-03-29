def180f12b4accca152f951395a2e50a24db2ba2
Lucas <genevasterm@protonmail.ch>
2021-03-29 02:06:18 -0500

    tweak: set AA to utilize 4 samples

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 #![windows_subsystem = "windows"]
 
-use bevy::prelude::{App, WindowDescriptor};
+use bevy::prelude::*;
 use bevy::DefaultPlugins;
 
 fn main() {
     App::build()
+        .insert_resource(Msaa { samples: 4 })
         .insert_resource(WindowDescriptor {
             title: "urban-telegram".to_string(),
             width: 1920.,
